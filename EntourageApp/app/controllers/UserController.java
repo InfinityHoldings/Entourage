876b902e42e9de3275536e442398224a91b167fe
damion6891 <damion6891@yahoo.com>
2014-11-08 18:21:02 -0500

    new feature [AWS]

--- EntourageApp/app/controllers/UserController.java
+++ EntourageApp/app/controllers/UserController.java
@@ -1,43 +1,53 @@
 package controllers;
 
 import java.util.*;
-
-
+import models.EntourageUser;
 
 import org.hibernate.*;
 import org.mindrot.jbcrypt.*;
-import java.lang.NullPointerException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import play.Logger;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import util.HibernateUtil;
 
 public class UserController extends Controller {
+	static Session session = null;
+	// private static boolean _validate = false;
+	// private static String _uname, _password;
+	// private static boolean _validate = false;
+	private static String _uname, _password;
 
-	private static boolean _validate = false;
-	private static String _uname, _passwordHash;
-
-	public static Result createUserAccount(String lname, String fname,
-			String birthDate, String title, String address1, String address2,
-			String city, String state, String postal, String phone,
-			String email, String password, String creationDate, String userName) {// Blob
-																					// picMedium,
-																					// Blob
-																					// picSmall,
-
+	public static Result signUp() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
+		JsonNode json = request().body().asJson();
 		Transaction tx = null;
+		ObjectNode result = Json.newObject();
+
 		try {
 			tx = session.beginTransaction();
-			EntourageUser entUser = new EntourageUser(lname, fname, birthDate,
-					title, address1, address2, city, state, postal, phone,
-					email, password, creationDate, userName);
-			entUser.setPassword(BCrypt.hashpw(password, BCrypt.gensalt()));
-			session.save(entUser);
+			String userName = json.findPath("username").textValue();
+			String email = json.findPath("email").textValue();
+			String city = json.findPath("city").textValue();
+			String state = json.findPath("state").textValue();
+			String password = json.findPath("password").textValue();
+
+			if (isExist(userName)) {
+				result.put("status", "0");
+				return ok("{UserExist:" + result + "}");
+			}
+			EntourageUser user = new EntourageUser(userName, email, city,
+					state, password);
+
+			Logger.debug("SignUp EntourageUser", user.toString());
+			session.save(user);
 			tx.commit();
+			result.put("status", "1");
+			return ok("{UserExist:" + result + "}");
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
@@ -45,7 +55,55 @@ public class UserController extends Controller {
 		} finally {
 			session.close();
 		}
-		return ok("User created!");
+		return null;
+	}
+
+	public static Result login() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = null;
+		JsonNode json = request().body().asJson();
+		ObjectNode result = Json.newObject();
+		if (json == null) {
+			return badRequest("Expecting Json data");
+		} else {
+			try {
+				String username = json.findPath("username").textValue();
+				String password = json.findPath("password").textValue();
+				tx = session.beginTransaction();
+				String sql = "Select * from ent_user where username = '"
+						+ username + "' and password = '" + password + "';";
+				SQLQuery q = session.createSQLQuery(sql);
+				q.addEntity(EntourageUser.class);
+				@SuppressWarnings("unchecked")
+				Iterator<EntourageUser> iterator = q.list().iterator();
+				if (!iterator.hasNext()) {
+					result.put("status", "User Not Found");
+					return ok("{User:" + result + "}");
+				} else
+					do {
+						EntourageUser entUser = iterator.next();
+						_uname = entUser.getUserName();
+						_password = entUser.getPassword();
+						
+						// _passwordHash = entUser.getPassword();
+						// _validate = authenticate(username, password,
+						// _passwordHash);
+						if (username.equalsIgnoreCase(_uname)
+								&& password.equals(_password)) {
+							result.put("status", "User Found");
+							return ok("{User:" + result + "}");
+						}
+					} while (iterator.hasNext());
+				tx.commit();
+			} catch (HibernateException e) {
+				if (tx != null)
+					tx.rollback();
+				e.printStackTrace();
+			} finally {
+				session.close();
+			}
+			return null;
+		}
 	}
 
 	public static Result getAllUsers() {
@@ -56,6 +114,7 @@ public class UserController extends Controller {
 			String sql = "Select * from ent_user";
 			SQLQuery q = session.createSQLQuery(sql);
 			q.addEntity(EntourageUser.class);
+			@SuppressWarnings("unchecked")
 			Iterator<EntourageUser> iterator = q.list().iterator();
 			while (iterator.hasNext()) {
 				EntourageUser entUser = iterator.next();
@@ -129,80 +188,31 @@ public class UserController extends Controller {
 		return null;
 	}
 
-	public static Result login() {
-		JsonNode json = request().body().asJson();
-		ObjectNode result = Json.newObject();
-		if (json == null) {
-			return badRequest("Expecting Json data");
-		} else {
-			String username = json.findPath("username").textValue();
-			String password = json.findPath("password").textValue();
-			if (username == null || password == null) {
-				result.put("status", "KO");
-				result.put("message", "Missing parameter [name]");
-				return badRequest(result);
-			} else {
-				result.put("status", "OK");
-				result.put("username", "Hello " + username);
-				result.put("password", password);
-				return ok(result);
-			}
-		}
-	}
-
-	public static Result login2(String username, String password) {
+	public static boolean isExist(String userName) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction tx = null;
+		boolean exist = false;
 		try {
 			tx = session.beginTransaction();
-			String sql = "Select * from ent_user where username = '" + username
-					+ "';";
+			String sql = "Select 1 FROM ent_user WHERE username = '" + userName
+					+ "'";
 			SQLQuery q = session.createSQLQuery(sql);
-			q.addEntity(EntourageUser.class);
+			@SuppressWarnings("unchecked")
 			Iterator<EntourageUser> iterator = q.list().iterator();
-			try {
-				while (iterator.hasNext()) {
-					EntourageUser entUser = iterator.next();
-					_uname = entUser.getUserName();
-					// _passwordHash = entUser.getPassword();
-					// _validate = authenticate(username, password,
-					// _passwordHash);
-
-					if (username.equalsIgnoreCase(_uname)) {
-						return ok("ok");
-					} else {
-						return ok("No");
-					}
-				}
-			} catch (NullPointerException ex) {
-				ex.printStackTrace();
-			}
-			tx.commit();
+			if (!iterator.hasNext())
+				exist = false;
+			else
+				exist = true;
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
 			e.printStackTrace();
 		} finally {
-
 			session.close();
 		}
-		return null;
+		return exist;
 	}
 
-	//@BodyParser.Of(BodyParser.Json.class)
-	public static Result loginbones(){
-		String uname = request().getQueryString("username"); 
-	//	String postname = request().body().asText(); 
-		//RequestBody body = request().body();
-	//	String 	requestString = body.toString();
-//		JsonNode node = request().body().asJson(); 
-//		String nodename = node.findPath("username").textValue(); 
-//		
-		//System.out.print("Request String = " + requestString + "\n" + "JSON = " + body.asText() + "\n"); //+ requestString + "\n"); // + postname.findValue("username"));
-		
-		return ok("Hello, "+ uname); 
-	}
-	
 	public static boolean authenticate(String userName, String password,
 			String passwordHash) {
 		if (userName != null && BCrypt.checkpw(password, passwordHash)) {
