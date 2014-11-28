c6045d4f1df44e8b277d940e9b4a4a58d0938349
KeonW <keontwilliams@gmail.com>
2014-11-28 18:09:19 -0500

    Created event class. committing before mergining in usercrud changes from UserCrudApi branch while waiting for pull requeset approval on dev

---
+++ EntourageApp/app/models/Event.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Event {
+	//Change eventtype to enum 
+	private String eventName, eventType; 
+
+	public Event(){
+		
+	}
+	
+	public Event(String eventName){
+		
+	}
+	
+	
+	private void setEventName(String eventName){
+		this.eventName = eventName; 
+	}
+	
+	private void setEventType(String eventName){
+		this.eventName = eventName; 
+	}
+}
